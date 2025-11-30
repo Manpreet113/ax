165a75d7a0ced709f9410df2b239044bbf3f8c84
Manpreet113 <manpreet10542@gmail.com>
2025-11-30 11:26:01 +0530

    Don't know why this file wasn't pushed with last commit!

---
+++ src/git_ops.rs
@@ -0,0 +1,55 @@
+use git2::build::RepoBuilder;
+use git2::{FetchOptions, Progress, RemoteCallbacks};
+use std::path::Path;
+
+/// Clones a repo from `url` to `path` with a progress reporter
+pub fn clone_repo(url: &str, path: &Path) -> Result<(), git2::Error> {
+    println!(":: Initializing clone for {}", url);
+
+    // 1. Set up the Callbacks
+    // These are hooks that trigger during the download process.
+    let mut callbacks = RemoteCallbacks::new();
+
+    // The 'transfer_progress' callback is triggered repeatedly during download.
+    // 'stats' contains the numbers (total objects, received objects, etc.)
+    callbacks.transfer_progress(|stats| {
+        if stats.received_objects() == stats.total_objects() {
+            // Avoid spamming stdout, only print when a step completes
+            print!(
+                "\r:: Resolving deltas {}/{}\r",
+                stats.indexed_deltas(),
+                stats.total_deltas()
+            );
+        } else if stats.total_objects() > 0 {
+            // Calculate percentage
+            let p = (stats.received_objects() as f64 / stats.total_objects() as f64) * 100.0;
+            print!(
+                "\r:: Downloading objects: {}/{} ({:.0}%)",
+                stats.received_objects(),
+                stats.total_objects(),
+                p
+            );
+        }
+        // Return true to continue the transfer. Returning false aborts it.
+        true
+    });
+
+    // 2. Set up Fetch Options
+    // We attach the callbacks to the fetch options.
+    let mut fetch_options = FetchOptions::new();
+    fetch_options.remote_callbacks(callbacks);
+
+    // 3. Build and Execute
+    // We use the Builder pattern to configure the clone operation
+    let mut builder = RepoBuilder::new();
+    builder.fetch_options(fetch_options);
+
+    // This line actually performs the network request and disk write
+    match builder.clone(url, path) {
+        Ok(_) => {
+            println!("\n:: Clone complete!");
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
+}
